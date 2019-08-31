dbe0107f8ee90704f4592c685f038d4ea6616e36
KIMBradlyCooper <wikis123@naver.com>
2019-08-31 20:22:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/naroho_1/ToolsLayout.java
@@ -0,0 +1,20 @@
+package com.example.naroho_1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ToolsLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.tools_layout,container,false);
+        return v;
+    }
+}
